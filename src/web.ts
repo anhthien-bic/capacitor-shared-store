1ecb9ca666b23c122ac3d5da7b6eccdd75322445
Nguyễn Anh Thiện <16835989+thienna@users.noreply.github.com>
2025-07-14 11:46:36 +0700

    mock web

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { WebPlugin } from '@capacitor/core';
 
 import type { SharedStorePlugin } from './definitions';
 
 export class SharedStoreWeb extends WebPlugin implements SharedStorePlugin {
-  getItem(options: { key: string; appGroup: string; }): Promise<{ value: string | null; }> {
+  getItem(_options: { key: string; appGroup: string; }): Promise<{ value: string | null; }> {
     throw new Error('Method not implemented.');
   }
-  setItem(options: { key: string; value: string; appGroup: string; }): Promise<void> {
+  setItem(_options: { key: string; value: string; appGroup: string; }): Promise<void> {
     throw new Error('Method not implemented.');
   }
 }
